72ca579955847116518c23cb147de3c06843752d
nexalphic <nathangarces1@gmail.com>
2017-02-13 10:14:50 -0800

    Update ccipher.py

--- ccipher.py
+++ ccipher.py
@@ -1,9 +1,16 @@
 letters = "abcdefghijklmnopqrstuvwxyz"
 def ccipher(encode, key):
-    crypted = ""
-    for ch in encode:
-        crypted = crypted + chr(ord(ch) + key)
-    print crypted
+    result = ""
+    mode = int(input("Would you like to (1)encrypt or (2)decrypt?"))
+    if mode == 1:
+        for ch in encode:
+            result = result + chr(ord(ch) + key)
+    elif mode == 2:
+        for ch in encode:
+            crypted = crypted + chr(ord(ch) - key)
+    else:
+        print "mode number not recognized"
+    print result
     
 #Example input:
-ccipher("hello", 1)
+print ccipher(input"Input string.")
