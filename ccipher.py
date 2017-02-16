ef514b21d75ca85286c5932e545d03b52ac8fb91
nexalphic <nathangarces1@gmail.com>
2017-02-16 14:55:34 -0800

    Update ccipher.py

--- ccipher.py
+++ ccipher.py
@@ -10,14 +10,16 @@ def ccipher():
     elif mode in [2, "decrypt", "e", "Decrypt", "E"]:
         key = int(input("Input a key."))
         for ch in encode:
-            crypted = crypted + chr(ord(ch) - key)
+            result = result + chr(ord(ch) - key)
     elif mode in [3, "brute force", "b", "brute", "force"]:
         times = int(input("How many times would you like to brute force?"))
         key = 1
         for times in range(times):
             for ch in encode:
-                crypted = crypted + chr(ord(ch) - key)
+                result = result + chr(ord(ch) - key)
+                result = ""
         key -= 1
+        print result
     else:
         print "mode number not recognized"
     print result
