b3189848173ed9cba6e7b341c8493e5fae1b73d9
nexalphic <nathangarces1@gmail.com>
2017-04-19 10:42:51 -0700

    Update RPSLS.py

--- RPSLS.py
+++ RPSLS.py
@@ -16,27 +16,30 @@ while gameOn == True:
     while playerChoice.lower() not in ["rock", "paper", "scissors", "lizard", "spock", "quit"]:
         playerChoice = input("Rock, Paper, Scissors, Lizard, or Spock?")
     computerChoice = random.choice(list(choices))
-    x = abs(choices[computerChoice] - choices[playerChoice]) % 5
     print "You chose " + playerChoice
     print "The computer chose " + computerChoice
     if playerChoice.lower() == "quit":
         break
-    elif x == 0:
-        print "It's a tie."
-    elif x == 2 or x == 1:
-        print "You win."
-        playerScore += 1
     else:
-        print "You lose."
-        playerscore -= 1
-    foundInput = False
-    while foundInput == False:
-        keepPlaying = input("Keep playing? Y/N").lower()
-        if keepPlaying in ["y", "n", "yes", "no"]:
-            if keepPlaying in ["y", "yes"]:
-                foundInput == True
-            elif keepPlaying in ["n", "no"]:
-                gameOn == False
-                break
-                print "Your score was: " + str(playerScore)
-                foundInput == True
+        x = abs(choices[computerChoice] - choices[playerChoice]) % 5
+        if x == 0:
+            print "It's a tie."
+        if x == 2 or x == 1:
+            print "You win."
+            playerScore += 1
+        else:
+            print "You lose."
+            playerScore -= 1
+        foundInput = False
+        while foundInput == False:
+            keepPlaying = input("Keep playing? Y/N").lower()
+            if keepPlaying in ["y", "n", "yes", "no"]:
+                if keepPlaying in ["y", "yes"]:
+                    foundInput == True
+                elif keepPlaying in ["n", "no"]:
+                    gameOn == False
+                    break
+                    print "Your score was: " + str(playerScore)
+                    foundInput == True
+            else:
+                print "That's not yes or no."
