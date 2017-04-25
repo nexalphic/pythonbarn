1a2cae355c66800b6309f684bbca87cc78dbf95b
nexalphic <nathangarces1@gmail.com>
2017-04-25 10:47:09 -0700

    Update tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -1,4 +1,5 @@
 import simplegui
+import random
 
 WIDTH = 600
 HEIGHT = 600
@@ -9,9 +10,19 @@ states = {
     2: "o"
 }
 
-}
+def wipe_grid():
+    global grid
+    grid = [
+        [0, 0, 0],
+        [0, 0, 0],
+        [0, 0, 0]
+        ]
 
-def mouse_handler(position):
+playing = "start"
+print_grid = False
+c_text = []
+
+def mouse_zone(position):
     zone = []
     for axis in (0, 1):
         if position[axis] < 200:
@@ -21,10 +32,24 @@ def mouse_handler(position):
         else:
             zone.append(3)
     return zone
+
+def center_text():
+    if playing == "start":
+        c_text = [210, "or", 15]
+    else:
+        c_text = [205, "wins", 11]
+
+def mouse_handler(position):
+    zone = mouse_zone(position)
+    if playing == "start":
+        c_text("start")
+        grid[1][0] = 1
+        grid[1][2] = 2
         
-def game():
-    playing = True
-    while playing == True:
+    
+        
+        
+
         
 
 frame = simplegui.create_frame("Tic Tac Toe", WIDTH, HEIGHT)
@@ -32,11 +57,16 @@ frame = simplegui.create_frame("Tic Tac Toe", WIDTH, HEIGHT)
 frame.set_mouseclick_handler(mouse_handler)
 
 def draw_handler(canvas):
-    canvas.draw_line((200, 20), (200, 580), 5, "White")
-    canvas.draw_line((400, 20), (400, 580), 5, "White")
-    canvas.draw_line((20, 200), (580, 200), 5, "White")
-    canvas.draw_line((20, 400), (580, 400), 5, "White")
-
+    
+    if print_grid == True:
+        canvas.draw_line((200, 20), (200, 580), 5, "White")
+        canvas.draw_line((400, 20), (400, 580), 5, "White")
+        canvas.draw_line((20, 200), (580, 200), 5, "White")
+        canvas.draw_line((20, 400), (580, 400), 5, "White")
+    for x in xrange(len(grid)):
+        for y in xrange(len(grid(x):
+			if grid[x][y] == "x":
+                            if x = 
 frame.set_draw_handler(draw_handler)
 
 
