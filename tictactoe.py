8f0d4c6fff822a195cecff34d6bd19b81e005fc5
nexalphic <nathangarces1@gmail.com>
2017-04-26 11:08:22 -0700

    Update tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -17,10 +17,13 @@ def wipe_grid():
         [0, 0, 0],
         [0, 0, 0]
         ]
-
+    
+wipe_grid()
 playing = "start"
 print_grid = False
 c_text = []
+player_piece = True
+#1 = X, 2 = O
 
 def mouse_zone(position):
     zone = []
@@ -33,41 +36,51 @@ def mouse_zone(position):
             zone.append(3)
     return zone
 
-def center_text():
-    if playing == "start":
+def center_text(text):
+    if text == "start":
         c_text = [210, "or", 15]
-    else:
+    elif text == "wins":
         c_text = [205, "wins", 11]
+    else:
+        c_text[1] = ""
 
 def mouse_handler(position):
+    global grid
     zone = mouse_zone(position)
     if playing == "start":
         c_text("start")
         grid[1][0] = 1
         grid[1][2] = 2
+        if zone == [1, 0]:
+            player_piece = 1
+        elif zone == [1, 2]:
+            player_piece = 2
+            playing = "game"
+    if playing == "game":
+        grid[zone[0]][zone[1]] = player_piece
+        
         
 def mark_draw(zone):
+    global grid
     if grid[x][y] == "x":
-    	if x == 0:
-        
-        
-
-        
-
+        canvas.draw_line((x*200+20, y*200+20), (x*200+180, y*200+180), 7, "White")
+    else:
+        canvas.draw_line((x*200+100, y*200+100), 80, 7, "White")
+            
 frame = simplegui.create_frame("Tic Tac Toe", WIDTH, HEIGHT)
 
 frame.set_mouseclick_handler(mouse_handler)
 
 def draw_handler(canvas):
-    
+    global grid
     if print_grid == True:
         canvas.draw_line((200, 20), (200, 580), 5, "White")
         canvas.draw_line((400, 20), (400, 580), 5, "White")
         canvas.draw_line((20, 200), (580, 200), 5, "White")
         canvas.draw_line((20, 400), (580, 400), 5, "White")
     for x in xrange(len(grid)):
-        for y in xrange(len(grid(x):
-            	mark_draw(grid[x][y], x, y)
+        for y in xrange(len(grid(x))):
+                mark_draw(grid[x][y], x, y)
                             
 frame.set_draw_handler(draw_handler)
 
