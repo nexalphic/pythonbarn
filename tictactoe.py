b4afb1b66dd25b0f0157181abcc8e7dc9131bcc3
nexalphic <nathangarces1@gmail.com>
2017-04-28 10:49:56 -0700

    Update tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -20,14 +20,13 @@ def wipeGrid():
     
 wipeGrid()
 playing = "start"
-print_grid = False
 c_text = []
 player_piece = True
 #1 = X, 2 = O
 
 def mouseZone(position):
     zone = []
-    for axis in (0, 1):
+    for axis in range(0, 2):
         if position[axis] < 200:
             zone.append(1)
         elif position[axis] < 400:
@@ -37,15 +36,18 @@ def mouseZone(position):
     return zone
 
 def centerText(text):
+    global c_text
     if text == "start":
-        c_text = [210, "or", 15]
+        c_text = [249, "or", 120]
     elif text == "wins":
-        c_text = [205, "wins", 11]
+        c_text = [205, "wins", 96]
     else:
         c_text[1] = ""
         
-grid[1][0] = 1
-grid[1][2] = 2
+centerText(playing)       
+    
+grid[1][2] = 1
+grid[1][0] = 2
 
 def mouse_handler(position):
     global grid
@@ -54,15 +56,19 @@ def mouse_handler(position):
     zone = mouseZone(position)
     if playing == "start":
         centerText("start")
-        if zone == [1, 0]:
+        if zone == [1, 2]:
             player_piece = 1
-        elif zone == [1, 2]:
+            print "hello"
+            playing = "game"
+        elif zone == [1, 0]:
             player_piece = 2
             playing = "game"
+            print "hello"
         wipeGrid()
-    if playing == "game":
-        if grid[zone[0]][zone[1]] not in [1, 2]:
-            grid[zone[0]][zone[1]] = player_piece
+        centerText("game")
+    elif playing == "game":
+        if grid[zone[1]][zone[0]] != 1 or grid[zone[1]][zone[0]] != 2:
+            grid[zone[1]][zone[0]] = player_piece
     
 frame = simplegui.create_frame("Tic Tac Toe", WIDTH, HEIGHT)
 
@@ -70,17 +76,19 @@ frame.set_mouseclick_handler(mouse_handler)
 
 def draw_handler(canvas):
     global grid
-    if print_grid == True:
-        canvas.draw_line((200, 20), (200, 580), 5, "White")
-        canvas.draw_line((400, 20), (400, 580), 5, "White")
-        canvas.draw_line((20, 200), (580, 200), 5, "White")
-        canvas.draw_line((20, 400), (580, 400), 5, "White")
+    global c_text
+    canvas.draw_line((200, 20), (200, 580), 5, "White")
+    canvas.draw_line((400, 20), (400, 580), 5, "White")
+    canvas.draw_line((20, 200), (580, 200), 5, "White")
+    canvas.draw_line((20, 400), (580, 400), 5, "White")
+    canvas.draw_text(c_text[1], (c_text[0], 330), c_text[2], "White")
     for y in xrange(len(grid)):
         for x in xrange(len(grid[y])):
             state = grid[y][x]
-            if state == "1":
+            if state == 1:
                 canvas.draw_line((x*200+20, y*200+20), (x*200+180, y*200+180), 7, "White")
-            elif state == "2":
+                canvas.draw_line((x*200+180, y*200+20), (x*200+20, y*200+180), 7, "White")
+            elif state == 2:
                 canvas.draw_circle((x*200+100, y*200+100), 80, 7, "White")
                             
 frame.set_draw_handler(draw_handler)
